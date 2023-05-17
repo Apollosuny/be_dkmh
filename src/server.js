3c72738c4bf93c4830e2f73930b05bbea7c95d19
apollo <baotrung06092003@gmail.com>
2023-05-17 10:51:18 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const handleCookie = require('./handleCookie');
+let a;
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(cookieParser());
+
+app.get('/get', (req, res) => {
+    // console.log(req.cookies.a);
+    res.json({ cookie: req.cookies.a });
+})
+
+app.post('', (req, res) => {
+    a = req.body;
+    // console.log(handleCookie(req.body));
+    res.cookie('a', handleCookie(req.body));
+    res.send('Successfully')
+})
+
+app.post('/register', (req, res) => {
+    console.log(req.body);
+})
+
+app.listen(5000, () => {
+    console.log('Server is running at http://localhost:5000');
+})
